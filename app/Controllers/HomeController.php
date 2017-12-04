db221edd5373c1abb04641093eb1cc50f82c3a79
Fran Delgado <fran@thinway.org>
2017-12-04 16:48:02 +0100

    Implementado el registro de usuarios.

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\User;
+use Sirius\Validation\Validator;
+
 class HomeController extends BaseController {
 
     /**
@@ -31,6 +34,44 @@ class HomeController extends BaseController {
     }
 
     public function postRegistro(){
+        $errors = [];
+        $validator = new Validator();
 
+        $validator->add('inputName:Nombre', 'required', [], 'El {label} es obligatorio');
+        $validator->add('inputName:Nombre', 'minlength', ['min' => 5], 'El {label} debe tener al menos 5 caracteres');
+        $validator->add('inputEmail:Email', 'required', [], 'El {label} es obligatorio');
+        $validator->add('inputEmail:Email', 'email', [], 'No es un email válido');
+        $validator->add('inputPassword1:Password', 'required', [], 'La {label} es requerida');
+        $validator->add('inputPassword1:Password', 'minlength', ['min' => 8], 'La {label} debe tener al menos 8 caracteres');
+        $validator->add('inputPassword2:Password', 'required', [], 'La {label} es requerida');
+        $validator->add('inputPassword2:Password', 'match', 'inputPassword1', 'Las passwords deben coincidir');
+
+        if($validator->validate($_POST)){
+            $user = new User();
+
+            $user->name = $_POST['inputName'];
+            $user->email = $_POST['inputEmail'];
+            $user->password = password_hash($_POST['inputPassword1'], PASSWORD_DEFAULT);
+
+            $user->save();
+
+            header('Location: '.BASE_URL);
+        }else{
+            $errors = $validator->getMessages();
+        }
+
+        return $this->render('auth/register.twig', ['errors' => $errors]);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
